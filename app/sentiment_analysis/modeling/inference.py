ae7b01fe561b372737fb1e7bf0b48f4a8c838263
kaumil <kaumil.trivedi97@gmail.com>
2020-09-25 11:28:54 +0530

    Added env file to gitignore

--- app/sentiment_analysis/modeling/inference.py
+++ app/sentiment_analysis/modeling/inference.py
@@ -1,5 +1,5 @@
-import tensorflow as tf
-from tensorflow.keras.preprocessing.sequence import pad_sequences
+# import tensorflow as tf
+# from tensorflow.keras.preprocessing.sequence import pad_sequences
 import os
 import json
 import requests
